85e942f8edc9aebd82315bcf0812b756c7083f44
codeweft <codeweft@gmail.com>
2013-10-24 17:22:32 +0530

    Adding cookbook template for configuration

--- cookbooks/configure/recipes/default.rb
+++ cookbooks/configure/recipes/default.rb
@@ -1,15 +1,8 @@
+#
 # Cookbook Name:: configure
 # Recipe:: default
-
-common = {:worker_processes => 4, :port => 9999, :conf => "/etc/nginx/nginx.conf"}
-
-service 'nginx' do
-  supports :status => true, :restart => true, :reload => true
-  action [:enable, :start]
-end
-
-template "#{common[:conf]}" do
-  source 'nginx.conf.erb'
-  variables common
-  notifies :reload, "service[nginx]"
-end
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
